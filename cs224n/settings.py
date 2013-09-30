093fb214549bed84621b56b015cf5d812d620469
Spence Green <spence@spencegreen.com>
2013-09-29 18:09:01 -0700

    Add style sheet

--- cs224n/settings.py
+++ cs224n/settings.py
@@ -1,10 +1,12 @@
 # Django settings for cs224n project.
 
+INSTALL_DIR='/usr/local/www/lms'
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Spence Green', 'spenceg@stanford.edu'),
+    ('Spence Green', 'spenceg@stanford.edu'),
 )
 
 MANAGERS = ADMINS
@@ -61,7 +63,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '%s/static' % (INSTALL_DIR)
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
